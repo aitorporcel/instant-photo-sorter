82f570ee844706cdb98f116c0fc8af40943c4e0b
aitorporcel <aitor.porcellaburu@gmail.com>
2024-02-27 19:41:39 +0100

    New version with documented code

--- Readme.md
+++ Readme.md
@@ -1,1 +1,94 @@
-## WIP
+# Cellphone Image Classification Project
+
+This project aims to classify a large collection of cellphone images into four specific categories. It's designed for individuals looking to organize and categorize images stored in their devices, particularly those accumulated through apps like WhatsApp. The application utilizes a fine-tuned ResNet50 model for image classification and offers two main functionalities: training your own model with custom data or using the pre-trained model to classify your images.
+
+## Features
+
+- Custom model training with user-provided image data.
+- Image classification into four predefined categories using a fine-tuned ResNet50 model.
+- A GUI for easy interaction with the application for classification tasks.
+- Command-line support for model training and customization.
+- TensorBoard integration for training visualization and monitoring.
+- Detailed evaluation metrics, including classification reports and confusion matrices.
+
+## Getting Started
+
+### Prerequisites
+
+Ensure you have the following installed:
+
+- Python 3.8+
+- PyTorch 1.8+
+- torchvision 0.9+
+- PIL (Pillow)
+- scikit-learn
+- PySide2 (for the GUI)
+- TensorBoard (for logging)
+
+### Installation
+
+1. Clone the repository to your local machine:
+   ```sh
+   git clone https://github.com/your-username/your-project-name.git
+   ```
+2. Navigate to the project directory and install the required dependencies:
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+### Training Your Own Model
+
+To train your own model with custom datasets, follow these steps:
+
+1. **Prepare Your Dataset**: Organize your images into folders named after the categories you wish to classify. For example, if categorizing images into 'Memes', 'Landscapes', 'People', and 'Various', each category should have its own folder containing the relevant images.
+
+2. **Train the Model**: Use the command-line interface to start the training process with your dataset. You can customize training parameters such as the number of epochs, batch size, and train/validation split ratio:
+
+   ```sh
+   python main.py --num_epochs 30 --batch_size 32 --train_split 0.8
+   ```
+
+3. **Monitor Training Progress**: TensorBoard logs training metrics like loss and accuracy. To view these metrics, run TensorBoard and navigate to the provided URL in your web browser:
+
+   ```sh
+   tensorboard --logdir runs/
+   ```
+
+4. **Evaluate the Model**: After training, the model is automatically evaluated on the validation and test sets. Results, including the classification report and confusion matrix, are displayed in the terminal.
+
+### Classifying Images with the Pre-trained or Custom Model
+
+To classify your images using the GUI:
+
+1. **Start the Application**: Run `main.py` without any arguments to launch the GUI.
+
+   ```sh
+   python main.py
+   ```
+
+2. **Select Source and Output Folders**: In the GUI, choose the folder containing the images you wish to classify and specify the output directory where classified images will be stored.
+
+3. **Classify Images**: Click the 'Classify Images' button to start the classification process. Classified images will be copied to subfolders in the output directory, organized by category.
+
+## Project Structure
+
+- `main.py`: The application's entry point.
+- `utils.py`: Contains utility functions for training and evaluation.
+- `dataset_modelo/`: Default directory for the training dataset.
+- `test_data/`: Default directory for the test dataset.
+- `models/`: Directory where trained models are saved.
+
+## Contributing
+
+Contributions are welcome! Please feel free to fork the project, make changes, and submit pull requests with new features or improvements.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Contact
+
+Add LinkedIn
+Project Link: [https://github.com/your-username/your-project-name](https://github.com/your-username/your-project-name)
--- main.py
+++ main.py
@@ -1,105 +1,78 @@
+import argparse
+import datetime
 import torch
-import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, random_split
-from torchvision import models
 import torch.nn as nn
 import torch.optim as optim
-
+from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
-import datetime
+from torchvision import models, transforms
+from torchvision.datasets import ImageFolder
+
 from utils import evaluate_model, train_model, evaluate_model_per_class
 
-# Current date
-now = datetime.datetime.now()
+def main(num_epochs: int, batch_size: int, train_split: float):
+    now = datetime.datetime.now()
+    experiment_name = f"experiment_{now.strftime('%d_%m_%Y_%H_%M_%S')}"
+    writer = SummaryWriter(f'runs/{experiment_name}')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}")
 
-# Use the data in the experiment name including the time
-experiment_name = f"experiment_{now.strftime('%d_%m_%Y_%H_%M_%S')}"
+    transform = transforms.Compose([
+        transforms.Lambda(lambda image: image.convert('RGB')),  # Convert image to RGB
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
 
-# Create a SummaryWriter to write logs to
-writer = SummaryWriter(f'runs/{experiment_name}')
+    transform_test = transforms.Compose([
+        transforms.Lambda(lambda image: image.convert('RGB')),  # Convert image to RGB
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
 
-# Configuración del dispositivo
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Device: {device}")
+    dataset = ImageFolder(root='dataset_modelo', transform=transform)
+    test_dataset = ImageFolder(root='test_data', transform=transform_test)
 
-# Transformaciones de los datos
-transform = transforms.Compose([
-    transforms.Lambda(lambda image: image.convert('RGB')),  # Convert image to RGB
-    transforms.Resize((224, 224)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+    train_size = int(train_split * len(dataset))
+    val_size = len(dataset) - train_size
 
-transform_test = transforms.Compose([
-    transforms.Lambda(lambda image: image.convert('RGB')),  # Convert image to RGB
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-# Cargar el dataset completo
-dataset = ImageFolder(root='dataset_modelo', transform=transform)
-test_dataset = ImageFolder(root='test_data', transform=transform_test)
+    model = models.resnet50(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
 
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 4)
+    for param in model.layer4.parameters():
+        param.requires_grad = True
 
-# Tamaños para dividir: por ejemplo, 70% entrenamiento, 15% validación, 15% prueba
-train_size = int(0.7 * len(dataset))
-val_size = len(dataset) - train_size
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam([
+        {'params': model.fc.parameters()},
+        {'params': model.layer4.parameters(), 'lr': 1e-5}
+    ], lr=0.0005)
 
-# Dividir el dataset
-train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    model_trained = train_model(model, criterion, optimizer, train_loader, val_loader,
+                                device, writer, num_epochs=num_epochs)
+    
+    evaluate_model_per_class(model_trained, val_loader, criterion, device, test_eval=False)
+    evaluate_model_per_class(model_trained, test_loader, criterion, device, test_eval=True)
 
-# Less batch size is usually good for regularization
-batch_size = 16 #initial 32
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train a model on a dataset.")
+    parser.add_argument("--num_epochs", type=int, default=20, help="Number of epochs for training.")
+    parser.add_argument("--batch_size", type=int, default=16, help="Batch size for training and validation.")
+    parser.add_argument("--train_split", type=float, default=0.7, help="Percentage of data to use for training (rest is for validation).")
 
-# Crear DataLoaders
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    args = parser.parse_args()
 
-# Cargar modelo preentrenado
-model = models.resnet50(pretrained=True)
-
-# Congelar los parámetros del modelo para no ser entrenados
-for param in model.parameters():
-    param.requires_grad = False
-
-# Personalizar la última capa para las 4 categorías
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, 4)
-
-# Unfreeze the last few layers
-for param in model.layer4.parameters():
-    param.requires_grad = True
-
-model = model.to(device)
-
-# Definir la función de pérdida y el optimizador
-criterion = nn.CrossEntropyLoss()
-#optimizer = optim.Adam(model.fc.parameters(), lr=0.0005) #Initial 0.001
-
-# Define the optimizer with specific parameters and learning rates
-optimizer = optim.Adam([
-    {'params': model.fc.parameters()},
-    {'params': model.layer4.parameters(), 'lr': 1e-5}
-], lr=0.0005)
-
-
-# Asegúrate de pasar `val_loader` a la función de entrenamiento
-model_trained = train_model(model, criterion, optimizer, train_loader, val_loader, device, writer, num_epochs=20)
-
-# Asumiendo que val_dataset es tu objeto Subset creado a partir de un ImageFolder
-# y val_loader se creó a partir de val_dataset
-
-# Después del entrenamiento, evaluar el modelo en el conjunto de validación
-evaluate_model_per_class(model_trained, val_loader, criterion, device, test_eval=False)
-
-# Después del entrenamiento, evaluar el modelo en el conjunto de testeo
-evaluate_model_per_class(model_trained, test_loader, criterion, device, test_eval=True)
-
-# Guardar el modelo entrenado
-#torch.save(model_trained.state_dict(), 'models/model_trained.pth')
+    main(num_epochs=args.num_epochs, batch_size=args.batch_size, train_split=args.train_split)
